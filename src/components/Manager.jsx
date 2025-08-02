5f4a895cac078d77a38696784cd18bedf3bb95d6
Aamir Rafique <amirrafique6400@gmail.com>
2025-08-02 18:48:16 +0500

    Navbar and Manager jsx components created, working on manager.jsx (continues): Add eye for password input, installed lucide- react icon library, now continue from usestate: form

---
+++ src/components/Manager.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react';
+import { EyeClosed, EyeIcon } from 'lucide-react'
+
+const Manager = () => {
+
+    const [passwordEye, setpasswordEye] = useState(EyeClosed);
+    const [form, setform] = useState({ site: '', username: '', password: '' });
+
+    const showPassword = () => {
+        if (passwordEye === EyeClosed) {
+            setpasswordEye(EyeIcon);
+        }
+        else {
+            setpasswordEye(EyeClosed);
+        }
+    }
+
+
+    const savePassword = () => {
+
+    }
+
+
+    return (
+        <>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+
+            <div className=' mx-60 flex  flex-col justify-center items-center gap-8 p-4' >
+                <div className='text-center'>
+                    <h1><span className='font-bold'>AR</span>PassWizard</h1>
+                    <p>Your own Password Manager</p>
+                </div>
+                <input value={form.site} type="text" placeholder='Enter Website URL' className='w-full rounded-2xl border text-center pt-1 pb-1 border-green-700  focus:ring-1 ring-green-400  duration-200 outline-none ' name="" id="" />
+                <div className='w-full flex gap-8 ' >
+                    <input value={form.username} type="text" placeholder='Input Username' className='p-1 w-full rounded-2xl border border-green-700  focus:ring-1 ring-green-400  duration-200 outline-none text-center' name="" id="" />
+
+                    <div className='relative '>
+                        <input value={form.password} type="text" placeholder='Input Password' className='p-1 w-full border-green-700  focus:ring-1 ring-green-400  duration-200 outline-none   rounded-2xl border pl-2' name="" id="" />
+                        <span className='absolute right-1 top-1.5 cursor-pointer' title={passwordEye === EyeClosed ? 'Show Password' : 'Hide Password'} onClick={showPassword} >
+                            {React.createElement(passwordEye)}
+                        </span>
+                    </div>
+                </div>
+                <button onClick={savePassword} className='flex justify-center items-center border rounded-2xl px-2 bg-green-400 cursor-pointer'><lord-icon
+                    src="https://cdn.lordicon.com/sbnjyzil.json"
+                    trigger="hover"
+                    stroke="bold"></lord-icon>Add Password</button>
+            </div>
+        </>
+    )
+}
+
+export default Manager
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-blue-300'>
+            <div className=' h-12 px-2 flex justify-between mx-40 items-center'>
+                <div className='font-bold text-[1.4rem]'>ARPass</div>
+                <ul className='flex gap-7'>
+                    <li><a href="#" target='_blank' rel='noopner'><lord-icon src="https://cdn.lordicon.com/jeuxydnh.json" trigger="hover" stroke="bold"></lord-icon></a></li>
+                    <li><a href="https://github.com/Aamir-Rafique" target='_blank' rel='noopner'><lord-icon src="https://cdn.lordicon.com/jjxzcivr.json" stroke="bold" trigger="hover"></lord-icon></a></li>
+                </ul>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
