5eb150fe6b9e0e16d220b8b66e667f77e5f2e9db
Aamir Rafique <amirrafique6400@gmail.com>
2025-08-03 17:29:08 +0500

    pkg- lucide-react installed + added a copy text functionality to copy text from table data

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import {  useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { Copy } from 'lucide-react';
+
 
 const Manager = () => {
 
@@ -15,7 +17,6 @@ const Manager = () => {
         }
     }, [])
 
-    
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -27,6 +28,11 @@ const Manager = () => {
         setPasswordArray([...passwordArray, form]);
         localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));  //save site, username and password in localStorage.
         console.log([...passwordArray, form]);
+    }
+
+    const copyText = (text) => {
+        alert("Item copied to clipboard: " + text);
+        navigator.clipboard.writeText(text);
     }
 
 
@@ -45,7 +51,7 @@ const Manager = () => {
                         <input value={form.username} onChange={handleChange} type="text" placeholder='Input Username' className='p-1 w-full rounded-2xl border border-green-700  focus:ring-1 ring-green-400  bg-white duration-200 outline-none text-center' name="username" id="" />
 
                         <div className='w-full'>
-                            <input  value={form.password} onChange={handleChange} type="password" placeholder='Input Password' className='p-1 w-full border-green-700  focus:ring-1 ring-green-400 bg-white  duration-200 outline-none   rounded-2xl border pl-2 text-center' name="password" id="" />
+                            <input value={form.password} onChange={handleChange} type="password" placeholder='Input Password' className='p-1 w-full border-green-700  focus:ring-1 ring-green-400 bg-white  duration-200 outline-none   rounded-2xl border pl-2 text-center' name="password" id="" />
                         </div>
                     </div>
                     <button type='submit' className='flex justify-center items-center border rounded-2xl px-2 bg-green-300 cursor-pointer'><lord-icon
@@ -68,15 +74,28 @@ const Manager = () => {
                             <tbody className='bg-green-100'>
                                 {passwordArray.map((item, index) => (
                                     <tr key={index}>
-                                        <td className='text-center w-32 border-2 py-1 border-white '><a href={item.site} target='_blank' rel='noopener'>{item.site}</a></td>
-                                        <td className='text-center w-32 border-2 py-1 border-white '>{item.username}</td>
-                                        <td className='text-center w-32 border-2 py-1 border-white '>{item.password}</td>
+                                        <td className='  w-32 border-2 py-1 border-white '>
+                                            <div className='flex items-center  justify-center text-center gap-2'>
+                                                <a href={item.site} target='_blank' rel='noopener'>{item.site}</a>{item.site === "" ? '' : <Copy className='cursor-pointer size-4' onClick={() => copyText(item.site)} />}   {/*used cond rend here to only prevent adding copy icon when there is no data in the column*/}
+                                            </div>
+                                        </td>
+                                        <td className='text-center w-32 border-2 py-1 border-white '>
+                                            <div className='flex items-center  justify-center text-center gap-2'>
+                                                {item.username} {item.username === "" ? '' : <Copy className='cursor-pointer size-4' onClick={() => copyText(item.username)} />}
+                                            </div>
+                                        </td>
+
+                                        <td className='text-center w-32 border-2 py-1 border-white '>
+                                            <div className='flex items-center  justify-center text-center gap-2'>
+                                                {item.password} {item.password === "" ? '' : <Copy className='cursor-pointer size-4' onClick={() => copyText(item.password)} />}
+                                            </div>
+                                        </td>
                                     </tr>
                                 ))}
                             </tbody>
                         </table>}
                 </div>
-            </div>
+            </div >
         </>
     )
 }
