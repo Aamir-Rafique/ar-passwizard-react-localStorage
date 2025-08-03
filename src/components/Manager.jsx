330aaf8364f49cc5dfbb94a0250b46c1ec21b5ef
Aamir Rafique <amirrafique6400@gmail.com>
2025-08-03 16:31:36 +0500

    UI Improvement
    Uninstalled lucide-react pkg, removed custome eye icons and showPassword function (because that functionality is already available for input tag of type=password.)

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-import { EyeClosed, EyeIcon } from 'lucide-react'
+import {  useState, useEffect } from 'react'
 
 const Manager = () => {
 
-    const [passwordEye, setpasswordEye] = useState(EyeClosed);
     const [form, setForm] = useState({ site: '', username: '', password: '' });
     const [passwordArray, setPasswordArray] = useState([]);
 
@@ -17,16 +15,7 @@ const Manager = () => {
         }
     }, [])
 
-
-
-    const showPassword = () => {
-        if (passwordEye === EyeClosed) {
-            setpasswordEye(EyeIcon);
-        }
-        else {
-            setpasswordEye(EyeClosed);
-        }
-    }
+    
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -43,7 +32,7 @@ const Manager = () => {
 
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
 
             <div className=' mx-60 flex  flex-col justify-center items-center gap-7 p-4 ' >
                 <div className='text-center'>
@@ -55,11 +44,8 @@ const Manager = () => {
                     <div className='w-full flex gap-8 ' >
                         <input value={form.username} onChange={handleChange} type="text" placeholder='Input Username' className='p-1 w-full rounded-2xl border border-green-700  focus:ring-1 ring-green-400  bg-white duration-200 outline-none text-center' name="username" id="" />
 
-                        <div className='relative '>
-                            <input value={form.password} onChange={handleChange} type="text" placeholder='Input Password' className='p-1 w-full border-green-700  focus:ring-1 ring-green-400 bg-white  duration-200 outline-none   rounded-2xl border pl-2' name="password" id="" />
-                            <span className='absolute right-1 top-1.5 cursor-pointer' title={passwordEye === EyeClosed ? 'Show Password' : 'Hide Password'} onClick={showPassword} >
-                                {React.createElement(passwordEye)}
-                            </span>
+                        <div className='w-full'>
+                            <input  value={form.password} onChange={handleChange} type="password" placeholder='Input Password' className='p-1 w-full border-green-700  focus:ring-1 ring-green-400 bg-white  duration-200 outline-none   rounded-2xl border pl-2 text-center' name="password" id="" />
                         </div>
                     </div>
                     <button type='submit' className='flex justify-center items-center border rounded-2xl px-2 bg-green-300 cursor-pointer'><lord-icon
@@ -69,7 +55,7 @@ const Manager = () => {
                 </form>
                 <div className='flex flex-col items-center w-full gap-5  mb-5'>
                     <h2 className='font-bold'>Your Passwords:</h2>
-                    {passwordArray.length === 0 ? <div>No passwords to show! Start adding passwords now.</div>
+                    {passwordArray.length === 0 ? <div>No passwords to show. Start adding passwords now!</div>
                         :
                         <table className="table-auto w-full rounded-md overflow-hidden ">
                             <thead className='bg-green-600 text-white '>
